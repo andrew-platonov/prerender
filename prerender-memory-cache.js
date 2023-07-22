1afd384c9a745fb9222657127107e36d745ebb1a
Andrew Platonov <andrew.platonov@protonmail.com>
2023-07-22 15:00:36 +0400

    Telegram bot added

--- prerender-memory-cache.js
+++ prerender-memory-cache.js
@@ -1,4 +1,10 @@
 var cacheManager = require('cache-manager');
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '6580099025:AAHPCIXg6ml5waXlm6lgutS5ZsklHh0Mf98';
+const chatId = -749894895;
+const bot = new TelegramBot(token, {polling: true});
+
 var stat = {
 	totalHits: 0, 
 	totalMisses: 0, 
@@ -24,26 +30,44 @@ function updateStat() {
 				});
 			});
 		});
-		console.log(`Stat updated!`);
+		console.log(`Stat updated.`);
 	}
 }
 
 function sendStat() {
 	console.log(`Statistics: ${JSON.stringify(stat)}`);
+	bot.sendMessage(chatId, getHumanStatistics());
 	stat.hits = 0;
 	stat.misses = 0;
 	stat.start = Date.now();
 }
 
+function getHumanStatistics() {
+	return `Cache: ${stat.keys} keys of ${ Math.round(stat.size / 1024 / 1024 * 10) / 10 } Mb.
+Total (started at ${new Date(stat.totalStart).toLocaleString('ru-ru')})
+- hits  : ${stat.totalHits}
+- misses: ${stat.totalMisses}
+	
+Last (started at ${new Date(stat.start).toLocaleString('ru-ru')})
+- hits  : ${stat.hits}
+- misses: ${stat.misses}`;
+}
+
+bot.on('message', (msg) => {
+	console.log(msg);
+	bot.sendMessage(msg.chat.id, `Message: ${JSON.stringify(msg)}\n\nCurrent statistics: ${JSON.stringify(stat)}`);
+	bot.sendMessage(msg.chat.id, getHumanStatistics());
+});
+
 module.exports = {
 
 	init: function() {
 		this.cache = cacheManager.caching({
-			store: 'memory', max: process.env.CACHE_MAXSIZE || 100, ttl: process.env.CACHE_TTL || 60 * 60 * 24 * 3 /*seconds*/
+			store: 'memory', max: process.env.CACHE_MAXSIZE || 1000, ttl: process.env.CACHE_TTL || 60 * 60 * 24 * 3 /*seconds*/
 		});
 		cache = this.cache;
-		setInterval(updateStat, 10000);
-		setInterval(sendStat, 60000);
+		setInterval(updateStat, (60 * 10 *  1) * 1000); // each 10 min
+		setInterval(sendStat  , (60 * 60 * 24) * 1000); // once a day
 	},
 
 	requestReceived: function(req, res, next) {
